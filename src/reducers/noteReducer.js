b5b6dfe64d0c1722f5eefd4c70cf09a135276955
Jordan <jordanrob709@gmail.com>
2022-05-30 17:10:40 +0300

    added test for toggle importance action and refactored new note test in noteReducer

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,15 +1,29 @@
 const noteReducer = (state = [], action) => {
+    /*
     if(action.type === 'NEW_NOTE') {
       //state.push(action.data) this is not allowed as it directly changes state
       state.concat(action.data)
       return state
     }
-    if(action.type === 'TOGGLE_IMPORTANCE'){
-     
-      state.forEach(d => d.id === action.data.id? () => {d.important = !d.important}:d)
+    */
+
+    switch(action.type){
+      case 'NEW_NOTE':
+        return state.concat(action.data)
+      case 'TOGGLE_IMPORTANCE':{
+        const id = action.data.id
+        const noteToChange = state.find(n => n.id === id)
+        const changedNote = {
+            ...noteToChange,
+            important: !noteToChange.imprtant
+        }
+        return state.map(note => 
+          note.id !== id? note : changedNote
+        )
+      }
+      default:
+        return state
     }
-  
-    return state
   }
 
   export default noteReducer
