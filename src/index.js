df3b35c2c838384d1746f1880f7da6d6b115f1d6
Jordan <jordanrob709@gmail.com>
2022-06-23 16:46:50 +0300

    modified app and added an uncontrolled form as well as action creators

--- src/index.js
+++ src/index.js
@@ -54,6 +54,8 @@ const noteReducer = (state = [], action) => {
 
 */
 
+/*
+
 const store = createStore(noteReducer)
 
 store.dispatch({
@@ -84,6 +86,65 @@ const App = () => {
           </li>
         )}
         </ul>
+    </div>
+  )
+}
+
+
+*/
+
+
+const store = createStore(noteReducer)
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+  
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
+
+const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note =>
+          <li
+            key={note.id} 
+            onClick={() => toggleImportance(note.id)}
+          >
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
